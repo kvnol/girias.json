c4dbcc16ecd34dabd003b47307cc605763227720
lucassouzamatos <lucas.ssouza.mmatos@gmail.com>
2020-03-05 15:04:25 -0300

    adding database migration

---
+++ api_consumer/README.md
@@ -0,0 +1,17 @@
+# Gírias.json
+JSON API com gírias da rua e seus significados.
+
+## API
+---
+### Instalação
+
+```bash
+pip install -r requirements.txt
+py __builder__.py
+```
+
+### Inicialização
+
+```bash
+uvicorn api.main:app --reload
+```
---
+++ api_consumer/__builder__.py
@@ -0,0 +1,31 @@
+import json
+from tinydb import TinyDB, Query
+
+db = TinyDB('./database.json')
+
+"""
+    Initialize JSON migration to database
+"""
+def run():
+    with open('../girias.json', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        for slang in data["slang"]:
+            for nSlang in normalize(slang):
+                print(nSlang)
+                db.insert(nSlang)
+
+"""
+    Normalize slang to database insert
+"""
+def normalize(data):
+    if isinstance(data["name"], list):
+        for name in data["name"]:
+            yield {
+                **data,
+                "name": name
+            }
+
+    yield data
+
+if __name__ == "__main__":
+    run()
